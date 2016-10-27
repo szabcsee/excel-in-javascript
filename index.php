1074d68a12ec26440f0d9e1235a70456e40da208
Szabolcs Bokonyi <szabolcs.bokonyi@lhsystems.com>
2016-10-27 13:35:57 +0200

    debugging for php
    ----------------------
    branch [[master]]

--- index.php
+++ index.php
@@ -1,8 +1,3 @@
 <?php
-    require __DIR__ . '/RestServer/RestServer.php';
-    require __DIR__ . '/controller/MainController.php';
-
-$server = new \RestServer\RestServer('debug');
-$server->addClass('MainController');
-$server->handle();
+    print_l('Hello World');
 ?>
