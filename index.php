44156f4242b312391441abb9842b385c7017a568
Szabolcs Bokonyi <szabolcs.bokonyi@lhsystems.com>
2016-10-27 13:30:32 +0200

    changed server require path
    ----------------------
    branch [[master]]

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
-    require __DIR__ . '/../RestServer/RestServer.php';
-    require __DIR__ . '/../controller/MainController.php';
+    require __DIR__ . '/RestServer/RestServer.php';
+    require __DIR__ . '/controller/MainController.php';
 
 $server = new \RestServer\RestServer('debug');
 $server->addClass('MainController');
