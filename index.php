c781b6bf894eba6c21d3a93a0ae731789d5bf98a
Szabolcs Bokonyi <szabolcs.bokonyi@lhsystems.com>
2016-11-08 11:46:29 +0100

    php rendering html
    ----------------------
    branch [[master]]

--- index.php
+++ index.php
@@ -7,8 +7,15 @@
     $server = new ServerRouting();
 	$path = $server->getPath();
 	if ($path) {
-		$mainpage = file_get_contents(__DIR__ . '/js/src/index.phtml');
+		$mainpage = file_get_contents(__DIR__ . '/js/src/index.html');
 		echo $mainpage;
+        echo '<script src="' . __DIR__ . '/js/src/EventDispatcher.js"></script>';
+        echo '<script src="' . __DIR__ . '/js/src/Model.js"></script>';
+        echo '<script src="' . __DIR__ . '/js/src//js/src/View.js"></script>';
+        echo '<script src="' . __DIR__ . '/js/src/Controller.js"></script>';
+        echo '<script src="' . __DIR__ . '/js/src/app.js"></script>';
+        echo '</body>';
+        echo '</html>';
 	}
 
 ?>
