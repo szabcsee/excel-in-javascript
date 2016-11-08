822d5056bd123968d92e6d32a9f5eb057218238b
Szabolcs Bokonyi <szabolcs.bokonyi@lhsystems.com>
2016-11-08 11:17:50 +0100

    routes optimized for heroku
    ----------------------
    branch [[master]]

--- index.php
+++ index.php
@@ -3,11 +3,11 @@
 
 	require __DIR__ . '/rest-server/server.php';
     require __DIR__ . '/controller/MainController.php';
-	
+
     $server = new ServerRouting();
 	$path = $server->getPath();
 	if ($path) {
-		$mainpage = file_get_contents(__DIR__ . '/js/src/index.html');
+		$mainpage = file_get_contents(__DIR__ . '/js/src/index.phtml');
 		echo $mainpage;
 	}
 
