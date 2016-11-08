db0452bade2210a597cbc8faf1b32e49ac90851e
Szabolcs Bokonyi <szabolcs.bokonyi@lhsystems.com>
2016-11-08 12:07:47 +0100

    path to resources
    ----------------------
    branch [[master]]

--- index.php
+++ index.php
@@ -1,7 +1,5 @@
 <?php
     ini_set('display_errors', 1);
-    $rootPath = $_SERVER['DOCUMENT_ROOT'];
-
 	require __DIR__ . '/rest-server/server.php';
     require __DIR__ . '/controller/MainController.php';
 
@@ -10,13 +8,6 @@
 	if ($path) {
 		$mainpage = file_get_contents(__DIR__ . '/js/src/index.html');
 		echo $mainpage;
-        echo '<script src="' . $rootPath . '/js/src/EventDispatcher.js"></script>';
-        echo '<script src="' . $rootPath . '/js/src/Model.js"></script>';
-        echo '<script src="' . $rootPath . '/js/src//js/src/View.js"></script>';
-        echo '<script src="' . $rootPath . '/js/src/Controller.js"></script>';
-        echo '<script src="' . $rootPath . '/js/src/app.js"></script>';
-        echo '</body>';
-        echo '</html>';
 	}
 
 ?>
