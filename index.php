a460651c3cfcc299b88b0f4f189c5adde327c500
Szabolcs Bokonyi <szabolcs.bokonyi@lhsystems.com>
2016-11-08 12:02:24 +0100

    rootpath
    ----------------------
    branch [[master]]

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
     ini_set('display_errors', 1);
+    $rootPath = $_SERVER['DOCUMENT_ROOT'];
 
 	require __DIR__ . '/rest-server/server.php';
     require __DIR__ . '/controller/MainController.php';
@@ -9,11 +10,11 @@
 	if ($path) {
 		$mainpage = file_get_contents(__DIR__ . '/js/src/index.html');
 		echo $mainpage;
-        echo '<script src="' . __DIR__ . '/js/src/EventDispatcher.js"></script>';
-        echo '<script src="' . __DIR__ . '/js/src/Model.js"></script>';
-        echo '<script src="' . __DIR__ . '/js/src//js/src/View.js"></script>';
-        echo '<script src="' . __DIR__ . '/js/src/Controller.js"></script>';
-        echo '<script src="' . __DIR__ . '/js/src/app.js"></script>';
+        echo '<script src="' . $rootPath . '/js/src/EventDispatcher.js"></script>';
+        echo '<script src="' . $rootPath . '/js/src/Model.js"></script>';
+        echo '<script src="' . $rootPath . '/js/src//js/src/View.js"></script>';
+        echo '<script src="' . $rootPath . '/js/src/Controller.js"></script>';
+        echo '<script src="' . $rootPath . '/js/src/app.js"></script>';
         echo '</body>';
         echo '</html>';
 	}
